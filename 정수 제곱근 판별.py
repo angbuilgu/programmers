53248731c95730a36bc7c9568ccf61488da95d1a
Angbuilgu <sksmsdlscks@gmail.com>
2022-01-03 22:14:10 +0900

    한줄

--- 정수 제곱근 판별.py
+++ 정수 제곱근 판별.py
@@ -5,3 +5,11 @@ def solution(n):
         return (nn+1)*(nn+1)
     else:
         return -1
+
+'''
+한줄
+import math
+def solution(n):
+    return (math.sqrt(n)+1)*(math.sqrt(n)+1) if int(math.sqrt(n))*int(math.sqrt(n)) == n else -1
+
+'''
